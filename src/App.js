e09cbbe67a038984c8e8b0bcb0d94c7486ddf2e9
octaviomtz <octaviomtz@hotmail.com>
2022-05-24 15:21:14 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TextBlock from '../src/components/TextBlock.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>measurements analysis</p>
+        <TextBlock />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TextBlock.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TextBlock() {
+    return (
+        <div className='text-container'>
+            <p className="name">features names</p>
+            <p className="mean">features mean</p>
+            <p className="std">features std</p>
+        </div>
+    );
+}
+
+export default TextBlock;
